3d70ffe3f914e8e87713ae0da051b88fc6d0bae5
hukai <huzhengen@163.com>
2019-09-12 16:28:53 +0800

    add components

--- getting-started/src/app/app.module.ts
+++ getting-started/src/app/app.module.ts
@@ -6,21 +6,25 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    TopBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductListComponent,
+      TopBarComponent,
+      ProductAlertsComponent
+   ],
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+      ])
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
