20a5cefb1ce7371cc195726a0206333f79129c4d
hukai <huzhengen@163.com>
2019-09-16 14:44:36 +0800

    Display a Heroes List

--- getting-started/src/app/tour-of-heroes/tour-of-heroes.component.ts
+++ getting-started/src/app/tour-of-heroes/tour-of-heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-tour-of-heroes',
@@ -12,9 +13,19 @@ export class TourOfHeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.selectedHero);
   }
 
 }
